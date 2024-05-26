214e45084a270fbdd22db3a7c016a0fe65df50c3
Sloth <austere4@gmail.com>
2024-05-26 10:15:17 +0300

    words

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 0;
-        int y = 1000;
+        int account = 0;
+        int refill = 1000;
         int bonus = 0;
-        if (y >= 1000) {
-            bonus = y / 100;
+        if (refill >= 1000) {
+            bonus = refill / 100;
         }
-        System.out.println("Итоговый счет:" + (x+y));
+        System.out.println("Итоговый счет:" + (account+refill));
         System.out.println("Количество бонусных рублей:" + bonus);
 
     }
